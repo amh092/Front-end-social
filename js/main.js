d913ac643c26ebd9fe7bf3349f7c61bf9ab6f685
amh092 <ahmedalhazza@Ahmeds-MacBook-Pro.local>
2021-07-12 22:48:19 +0300

    some js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+// declare variables 
 
+var switchBtn = document.querySelector("button");
+var bodyTag = document.querySelector("body");
+var boxes = document.querySelector(".box")
+
+switchBtn.addEventListener("click", function () {
+    switchBtn.classList.toggle("switch-btn");
+    boxes.classList.toggle("dark-mode");
+})
