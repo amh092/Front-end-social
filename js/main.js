2a77ac4b17386e064cdacde5ffc612440a47066d
amh092 <ahmedalhazza@Ahmeds-MacBook-Pro.local>
2021-07-13 00:04:04 +0300

    what js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
 // declare variables 
 
 var switchBtn = document.querySelector("button");
-var bodyTag = document.querySelector("body");
-var boxes = document.querySelector(".box")
+var bodyTag = document.querySelectorAll(".first-container");
+var boxes = document.querySelectorAll(".box");
+var todayBoxes = document.querySelectorAll(".today-box");
+var midH1 = document.querySelectorAll(".mid h1:first-of-type")
 
 switchBtn.addEventListener("click", function () {
+    bodyTag[0].classList.toggle("background-black")
     switchBtn.classList.toggle("switch-btn");
-    boxes.classList.toggle("dark-mode");
+    for(i = 0; i < boxes.length; i = i +1 ) {
+        boxes[i].classList.toggle("dark-mode")
+       
+    }
+    for(i = 0; i < todayBoxes.length; i = i +1 ) {
+        todayBoxes[i].classList.toggle("dark-mode")
+    }
+    for(i = 0; i < midH1.length; i = i +1 ) {
+        midH1[i].classList.toggle("white-text")
+    }
+   
+    
 })
