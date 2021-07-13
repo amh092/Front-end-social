13593b7f32aaf9737ab06b9d950f722bfa89a970
amh092 <ahmedalhazza@Ahmeds-MacBook-Pro.local>
2021-07-13 08:58:35 +0300

    aha

--- js/main.js
+++ js/main.js
@@ -1,24 +1,28 @@
 // declare variables 
 
 var switchBtn = document.querySelector("button");
-var bodyTag = document.querySelectorAll(".first-container");
+var bodyTag = document.querySelectorAll("body");
 var boxes = document.querySelectorAll(".box");
 var todayBoxes = document.querySelectorAll(".today-box");
-var midH1 = document.querySelectorAll(".mid h1:first-of-type")
+var midH1 = document.querySelectorAll(".mid h1")
+var firstContainer = document.querySelector(".first-container")
+var buttonSpan = document.querySelector("button span")
+
+// toggling dark mode on and off based on button clicking
 
 switchBtn.addEventListener("click", function () {
-    bodyTag[0].classList.toggle("background-black")
+    bodyTag[0].classList.toggle("background-black");
+    bodyTag[0].classList.toggle("white-text");
     switchBtn.classList.toggle("switch-btn");
+    firstContainer.classList.toggle("dark-mode");
+    buttonSpan.classList.toggle("mode-switch-light")
+    buttonSpan.classList.toggle("switch-btn-span-dark")
     for(i = 0; i < boxes.length; i = i +1 ) {
         boxes[i].classList.toggle("dark-mode")
-       
     }
     for(i = 0; i < todayBoxes.length; i = i +1 ) {
         todayBoxes[i].classList.toggle("dark-mode")
     }
-    for(i = 0; i < midH1.length; i = i +1 ) {
-        midH1[i].classList.toggle("white-text")
-    }
-   
-    
+ 
 })
+
