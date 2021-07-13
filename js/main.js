8678f0a0ad4b01d0ab62913574bb6860f2a8b5ef
amh092 <ahmedalhazza@Ahmeds-MacBook-Pro.local>
2021-07-13 15:35:29 +0300

    TECO twice

--- js/main.js
+++ js/main.js
@@ -9,10 +9,16 @@ var midH1 = document.querySelectorAll(".black-text")
 var firstContainer = document.querySelector(".first-container")
 var buttonSpan = document.querySelector("button span")
 var overView = document.querySelector(".overview")
+var coder = document.querySelectorAll(".coder");
 
 // toggling dark mode on and off based on button clicking
 
 switchBtn.addEventListener("click", function () {
+    for(i =0; i < coder.length; i = i +1){
+        coder[i].classList.toggle("coder-name-blue")
+        coder[i].classList.toggle("coder-name-white")
+    }
+    
     bodyTag[0].classList.toggle("background-black");
     bodyTag[0].classList.toggle("white-text");
     switchBtn.classList.toggle("switch-btn");
@@ -27,11 +33,14 @@ switchBtn.addEventListener("click", function () {
     }
  
    for (i = 0; midH1.length; i = i + 1){
-   midH1[i].classList.toggle("black-text");
+    midH1[i].classList.toggle("black-text");
     midH1[i].classList.toggle("white-text");
 
      }
      overView.classList.toggle("gray-color")
      overView.classList.toggle("white-text")
+     
+
+
 })
 
